0f11d7466a56d5f5808e38a35d08bad34cec8267
k-masunaga <masumath84@gmail.com>
2020-07-09 17:57:40 +0900

    本番環境での環境変数読み込みの設定

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -4,9 +4,9 @@ class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
-  process resize_to_fit: [100, 100]
+  process resize_to_fit: [70, 70]
+  # storage :file
+  storage :fog
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
